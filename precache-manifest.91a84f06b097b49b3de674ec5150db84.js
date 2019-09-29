b4061338eb4c4fb775cd480e52ff57ab4ed01def
iasjem <jemimah.beryl.sai@gmail.com>
2019-09-29 22:04:28 +0800

    Updates

---
+++ precache-manifest.91a84f06b097b49b3de674ec5150db84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea3a0b9d8cdfae1253d08b55e4dbd87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58067046522a4a011112",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c489c9ded8c3ae3e081a",
+    "url": "/static/js/2.f4af569a.chunk.js"
+  },
+  {
+    "revision": "58067046522a4a011112",
+    "url": "/static/js/main.0e8ef54a.chunk.js"
+  },
+  {
+    "revision": "4a26d54245c85f2d2b06",
+    "url": "/static/js/runtime-main.6fc3339c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
